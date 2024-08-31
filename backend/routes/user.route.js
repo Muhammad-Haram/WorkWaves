2061fcfc8add24d53b203323af885662be56d988
Muhammad-Haram <haramh643@gmail.com>
2024-08-31 12:10:09 +0500

    route and middleware

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,1 +1,11 @@
 import express from "express";
+import { login, register, updateProfile } from "../controllers/user.controller";
+import isAuth from "../middlewares/isAuth";
+
+const router = express.Router();
+
+router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/profile/update").post(isAuth, updateProfile);
+
+export default router;
