2db9b75099d261eb2cf6bec99611507c3763b15d
Muhammad-Haram <haramh643@gmail.com>
2024-09-07 17:56:06 +0500

    create some sections

---
+++ frontend/src/components/CategoryCarousel.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
+import { Button } from "./ui/button";
+
+const category = [
+  "Frontend Developer",
+  "Backend Developer",
+  "Data Science",
+  "Graphic Designer",
+  "FullStack Developer",
+];
+const CategoryCarousel = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <Carousel className="w-full max-w-xl mx-auto my-20">
+        <CarouselContent>
+          {category.map((cate, index) => (
+            <CarouselItem className="text-center md:basis-1/3 lg-basis-1/3">
+              <Button variant="outline" className="rounded-full">
+                {cate}
+              </Button>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+};
+
+export default CategoryCarousel;
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import Navbar from "./shared/Navbar";
+import HeroSection from "./HeroSection";
+import LatestJobs from "./LatestJobs";
+import Footer from "./Footer";
+import CategoryCarousel from "./CategoryCarousel";
 
 const Home = () => {
   return (
-    <Navbar/>
+    <>
+      <Navbar />
+      <HeroSection />
+      <CategoryCarousel />
+      {/* <LatestJobs /> */}
+      {/* <Footer /> */}
+    </>
   );
 };
 
